880e8e8fe4a99b3da54d7d4fdfaa49304bfb20d3
bzanetis <britt.zanetis19@gmail.com>
2019-01-17 14:21:05 -0600

    Imported Search and VideoPlayer to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import VideoList from './VideoList.js';
-// BRITT I fixed the import here.  Line 15 works now
+import VideoPlayer from './VideoPlayer.js'
+import Search from './Search.js'
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer />
       </div>
       <div className="col-md-5">
       <VideoList />
